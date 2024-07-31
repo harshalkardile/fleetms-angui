8b10f811a80085727614a973a694df342d3ec787
harshal <kardileharshal567@gmail.com>
2024-07-31 20:49:00 +0530

    fetched coutries api

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-country',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './country.component.html',
   styleUrl: './country.component.css'
 })
-export class CountryComponent {
+export class CountryComponent implements OnInit {
+  countries: any[] = [];
 
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchCountries();
+  }
+
+  fetchCountries() {
+    this.httpClient.get<any[]>('http://localhost:8080/api/countries').subscribe({
+      next: (data) => {
+        this.countries = data;
+        console.log('Countries fetched:', this.countries);
+      },
+      error: (error) => {
+        console.error('Error fetching countries:', error);
+      }
+    });
+  }
 }
